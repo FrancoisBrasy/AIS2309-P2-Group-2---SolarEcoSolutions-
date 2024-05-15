229d5608315ff9f3864c2f9a10f0f1536a561537
Lucas DEGRET <126179037+MrCelsus@users.noreply.github.com>
2024-05-15 17:15:44 +0200

    Add files via upload

---
+++ Serveur-ad/Group.ps1
@@ -0,0 +1,47 @@
+﻿# Importation du module Active Directory
+Import-Module ActiveDirectory
+
+# Chemin vers le fichier CSV
+$CSVFile = "$env:USERPROFILE\Desktop\Liste_User.csv"
+
+# Importation des données utilisateur depuis le fichier CSV
+$CSVData = Import-CSV -Path $CSVFile
+
+# Table de correspondance entre le service et le CN du groupe
+$serviceToGroupMapping = @{
+    "R&D/Qualité" = "CN=R&D,OU=Groupe_Utilisateurs,OU=SolarEcoSolution,DC=SolarEcoSolution,DC=lan"
+    "Direction" = "CN=DG,OU=Groupe_Utilisateurs,OU=SolarEcoSolution,DC=SolarEcoSolution,DC=lan"
+    "Finances" = "CN=DAF,OU=Groupe_Utilisateurs,OU=SolarEcoSolution,DC=SolarEcoSolution,DC=lan"
+    "Service commercial" = "CN=Commerciaux,OU=Groupe_Utilisateurs,OU=SolarEcoSolution,DC=SolarEcoSolution,DC=lan"
+    "Informatique" = "CN=IT,OU=Groupe_Utilisateurs,OU=SolarEcoSolution,DC=SolarEcoSolution,DC=lan"
+    "Production" = "CN=Production,OU=Groupe_Utilisateurs,OU=SolarEcoSolution,DC=SolarEcoSolution,DC=lan"
+    "Logistique/approvisionnement" = "CN=Logistique,OU=Groupe_Utilisateurs,OU=SolarEcoSolution,DC=SolarEcoSolution,DC=lan"
+    "Expédition" = "CN=Logistique,OU=Groupe_Utilisateurs,OU=SolarEcoSolution,DC=SolarEcoSolution,DC=lan"
+    "Maintenance" = "CN=Maintenance,OU=Groupe_Utilisateurs,OU=SolarEcoSolution,DC=SolarEcoSolution,DC=lan"
+    "Atelier" = "CN=Atelier,OU=Groupe_Utilisateurs,OU=SolarEcoSolution,DC=SolarEcoSolution,DC=lan"
+    "Dpt Administratif & Financier" = "CN=DAF,OU=Groupe_Utilisateurs,OU=SolarEcoSolution,DC=SolarEcoSolution,DC=lan"
+}
+
+# Ajout des utilisateurs aux groupes
+foreach ($Utilisateur in $CSVData) {
+    $Firstname = $Utilisateur.PRENOM
+    $Lastname = $Utilisateur.NOM
+    $Service = $Utilisateur.SERVICE
+    $SamAccountName = "$Firstname.$Lastname".ToLower()
+    
+    # Récupération du DN du groupe basé sur la table de correspondance
+    $GroupDN = $serviceToGroupMapping[$Service]
+
+    If (-not $GroupDN) {
+        Write-Host "Aucun groupe trouvé pour le service : $Service" -ForegroundColor Red
+        Continue
+    }
+
+    Try {
+        # Tentative d'ajout de l'utilisateur au groupe
+        Add-ADGroupMember -Identity $GroupDN -Members $SamAccountName
+        Write-Host "Utilisateur $SamAccountName ajouté au groupe $GroupDN" -ForegroundColor Green
+    } Catch {
+        Write-Host "Erreur lors de l'ajout de l'utilisateur $SamAccountName au groupe $GroupDN : $_" -ForegroundColor Red
+    }
+}
---
+++ Serveur-ad/Users.ps1
@@ -0,0 +1,30 @@
+﻿# Importation du module Active Directory
+Import-Module ActiveDirectory
+
+# Chemin vers le fichier CSV
+$CSVFile = "$env:USERPROFILE\Desktop\Liste_User.csv"
+
+# Importation des données utilisateur depuis le fichier CSV
+$CSVData = Import-CSV -Path $CSVFile
+
+# Chemin par défaut pour la création des utilisateurs
+$DefaultOUPath = "OU=Utilisateurs,OU=SolarEcoSolution,DC=SolarEcoSolution,DC=lan"
+
+# Création des utilisateurs
+foreach ($Utilisateur in $CSVData) {
+$Firstname = [Text.Encoding]::ASCII.GetString([Text.Encoding]::GetEncoding("Cyrillic").GetBytes($Utilisateur.PRENOM.ToLower()))
+$Lastname = [Text.Encoding]::ASCII.GetString([Text.Encoding]::GetEncoding("Cyrillic").GetBytes($Utilisateur.NOM.ToLower()))
+
+    $SamAccountName = "$Firstname.$Lastname".ToLower()
+    $UserPrincipalName = "$SamAccountName@$DomainName.lan"
+    $DisplayName = "$Lastname $Firstname"
+    $Password = ConvertTo-SecureString "InitialPassword123!" -AsPlainText -Force
+
+    Try {
+        # Tentative de création de l'utilisateur
+        New-ADUser -SamAccountName $SamAccountName -UserPrincipalName $UserPrincipalName -Name $DisplayName -GivenName $Firstname -Surname $Lastname -Enabled $True -DisplayName $DisplayName -AccountPassword $Password -ChangePasswordAtLogon $True -Path $DefaultOUPath
+        Write-Host "Utilisateur créé : $DisplayName" -ForegroundColor Green
+    } Catch {
+        Write-Host "Erreur lors de la création de l'utilisateur $DisplayName : $_" -ForegroundColor Red
+    }
+}
